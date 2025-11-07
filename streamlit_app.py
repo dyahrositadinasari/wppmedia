82282c20ee962bcd7801c3886529651663a3fa2f
Dyah Dinasari <67815956+dyahrositadinasari@users.noreply.github.com>
2025-11-07 13:38:37 +0700

    Rename title and update introductory text

---
+++ streamlit_app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+#from streamlit_option_menu import option_menu
+
+st.title("POC Automation")
+st.write("xxx xxx xxx")
+st.logo(
+    "image/goat_logo.png", size="medium", link=None, icon_image=None
+)
